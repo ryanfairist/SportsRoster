58b1489a5f0487eec8550e41bd9132242c9052cf
RyanFairist <ryanfairistrichard@gmail.com>
2016-02-08 22:44:58 -0500

    add controller information

---
+++ js/controllers/playerBioController.js
@@ -0,0 +1,115 @@
+app.controller('playerBioController',['$scope' function($scope){
+	$scope.bios=[
+		{
+		game:{
+			sport:'Soccer',
+			name:'Rose Gillbert',
+			height: "5'4",
+			position:'Forward',
+			hometown:'Reisterstown, MD'
+			}
+		},
+
+		{
+		game:{
+			sport:'Basketball',
+			name:'Sierra Cohen',
+			height: "5'5",
+			position:'Center',
+			hometown:'Owings Mills, MD'
+			}
+		},
+
+		{
+		game:{
+			sport:'BobSled',
+			name:'Bridget Jackson',
+			height: "5'2",
+			position:'Runner',
+			hometown:'Anchorage,AK'
+			}
+		},
+
+		{
+		game:{
+			sport:'Boxing',
+			name:'Landania Tomlinson',
+			height: "5'6",
+			position:'Tough',
+			hometown:'Lancaster,PA'
+			}
+		},
+
+		game:{
+			sport:'Cheerleader',
+			name:'Paytonya Manning',
+			height: "5'5",
+			position:'Base',
+			hometown:'Devener, CO'
+			}
+		},
+
+        game:{
+			sport:'Golf',
+			name:'Rayna Lewis',
+			height: "5'7",
+			position:'Swinger',
+			hometown:'Baltimore, MD'
+			}
+		},
+
+		game:{
+			sport:'RollerSkating',
+			name:'Donna McNab',
+			height: "5'2",
+			position:'Speed Foward',
+			hometown:'Philidephia, PA'
+			}
+		},
+
+		game:{
+			sport:'Ballerina',
+			name:'Josephine Nameth',
+			height: "5'8",
+			position:'Lead Ballerina',
+			hometown:'East Rutherford, NJ'
+			}
+		},
+
+		game:{
+			sport:'Softball',
+			name:'Erin Rogers',
+			height: "5'6",
+			position:'Pitcher',
+			hometown:'Greenbay, WI'
+			}
+		},
+
+		game:{
+			sport:'Swimming',
+			name:'Tonya Brady',
+			height: "5'7",
+			position:'Diver',
+			hometown:'New England, MA'
+			}
+		},
+
+		game:{
+			sport:'Outdoor Track',
+			name:'Ednna Reed',
+			height: "5'6",
+			position:'Hurdler',
+			hometown:'Baltimore, MD'
+			}
+		},
+
+		game:{
+			sport:'Volley Ball',
+			name:'Casandra Newton',
+			height: "5'8",
+			position:'Setter',
+			hometown:'Charolett, NC'
+			}
+		}
+	]
+}]);
